53e4d044edb8c5c83bb7aeb51e3f09997e6ed7f4
金宏翔 <jinhongxiang01@daojia-inc.com>
2020-08-23 03:31:21 +0800

    提交一个hello文件

---
+++ src/com/imooc/com/gupao/Hello.java
@@ -0,0 +1,14 @@
+package com.imooc.com.gupao;
+
+/**
+ * @Description
+ * @Author jinhongxiang01
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2020/8/23
+ */
+public class Hello {
+    public String get() {
+        return "hello";
+    }
+}
